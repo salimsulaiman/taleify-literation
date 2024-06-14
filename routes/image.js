2387690cffea93b8979c1115d3e40ef0edd2c3ce
salimbahresy <pentingn1@gmail.com>
2024-06-14 11:29:44 +0700

    change image

--- routes/image.js
+++ routes/image.js
@@ -22,7 +22,7 @@ router.post("/upload_avatar/:id", upload.single("profilePicture"), async (req, r
     const updatedUser = await User.findByIdAndUpdate(
       id,
       {
-        picture: `http://localhost:3030/image/profile/${filename}`,
+        picture: `https://taleify-literation-production.up.railway.app/image/profile/${filename}`,
       },
       { new: true }
     );
